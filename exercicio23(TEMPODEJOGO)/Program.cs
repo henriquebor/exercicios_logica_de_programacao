652c764b55411867e1e07a29ba1d5d4f8d85f783
Henrique Borges <henrique.borges.hb79@gmail.com>
2023-03-02 21:18:29 -0300

    atualizacaodocodigo

--- exercicio23(TEMPODEJOGO)/Program.cs
+++ exercicio23(TEMPODEJOGO)/Program.cs
@@ -10,6 +10,23 @@ namespace exercicio23_TEMPODEJOGO_
     {
         static void Main(string[] args)
         {
+            int horaInicial, horaFinal, T;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(vet[0]);
+            horaFinal = int.Parse(vet[1]);
+
+            if (horaInicial < horaFinal)
+            {
+                T = horaFinal - horaInicial;
+                Console.WriteLine("O JOGO DUROU " + T + " HORA(S)");
+            }
+            else
+            {
+                T = (24 - horaInicial) + horaFinal;
+                Console.WriteLine("O JOGO DUROU " + T + " HORA(S)");
+            }
+            Console.ReadLine();
         }
     }
 }
