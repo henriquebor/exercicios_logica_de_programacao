94dab01c125960fd7ee72d6b0d0c81447acf061a
Henrique Borges <henrique.borges.hb79@gmail.com>
2023-03-04 22:01:29 -0300

    atualização de exercicio

---
+++ exercicio29(TEMPODEJOGO)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio29_TEMPODEJOGO_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, minutoInicial, horaFinal, minutoFinal;
+
+            string[] tempo = Console.ReadLine().Split(' ');
+
+            horaInicial = int.Parse(tempo[0]);
+            minutoInicial = int.Parse(tempo[1]);
+            horaFinal = int.Parse(tempo[2]);
+            minutoFinal = int.Parse(tempo[3]);
+
+            int instanteInicial = horaInicial * 60 + minutoInicial;
+            int instanteFinal = horaFinal * 60 + minutoFinal;
+
+            int duracao;
+
+            if (instanteInicial < instanteFinal)
+            {
+                duracao = instanteFinal - instanteInicial;
+            }
+            else
+
+            {
+                duracao = (24 * 60 - instanteInicial) + instanteFinal;
+            }
+
+            int duracaohoras = duracao / 60;
+            int duracaoMinutos = duracao % 60;
+
+            Console.WriteLine("O JOGO DUROU " + duracaohoras + " HORAS(S) E " + duracaoMinutos + " MINUTO(S)");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercicio30(ANIMAL)/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio30_ANIMAL_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipo, especie, alimentacao;
+
+            tipo = Console.ReadLine();
+            especie = Console.ReadLine();
+            alimentacao = Console.ReadLine();
+
+            if (tipo == "vertebrado")
+            {
+                if (especie == "ave")
+                {
+                    if (alimentacao == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else if (alimentacao == "onivoro")
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else if (especie == "mamifero")
+                {
+                    if (alimentacao == "onivoro")
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else if (alimentacao == "herbivoro")
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else if (tipo == "invertebrado")
+            {
+                if (especie == "inseto")
+                {
+                    if (alimentacao == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else if (alimentacao == "herbivoro")
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else if (especie == "anelideo")
+                {
+                    if (alimentacao == "hemtafogo")
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else if (alimentacao == "onivoro")
+                    {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+            Console.ReadLine();
+         }
+
+     }
+}
+
