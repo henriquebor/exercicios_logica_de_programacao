5014360421bd3f7ee8b1e0e618cebbdefeccb525
Henrique Borges <henrique.borges.hb79@gmail.com>
2023-03-14 17:49:21 -0300

    Atualização

---
+++ ESTRUTURA(WHILE)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESTRUTURA_WHILE_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+
+            x = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            while (x!= 0)
+            {
+                soma += x;
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercicio35(TABUADA)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio35_TABUADA_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = i * n;
+                Console.WriteLine(i + " x " + n + " = " + resultado);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
