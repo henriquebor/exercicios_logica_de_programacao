30bd05316398b445dcf825752dcd6226e3c776a9
Henrique Borges <henrique.borges.hb79@gmail.com>
2023-03-22 21:09:25 -0300

    add

--- exercicio(VETOR-QUESTAO-5)/Program.cs
+++ exercicio(VETOR-QUESTAO-5)/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Globalization;
 namespace exercicio_VETOR_QUESTAO_5_
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            int n = int.Parse(Console.ReadLine());
+
+            string[] convert = Console.ReadLine().Split(' ');
+
+            int[] Pares = new int[n];
+
+            for (int i=0; i<n; i++)
+            {
+                Pares[i] = int.Parse(convert[i]);
+            }
+            int cont = 0;
+            int resto;
+            int resul;
+            int soma = 0;
+            for (int i=0; i<n; i++)
+            {
+                resto = Pares[i] % 2;
+                if (resto == 0)
+                {
+                    soma = soma + Pares[i];
+                    cont++;
+                }
+            }
+            resul = soma / cont;
+
+            Console.WriteLine(resul.ToString("F1", CultureInfo.InvariantCulture));
         }
     }
 }
---
+++ exercicio(VETOR-QUESTAO-6)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_VETOR_QUESTAO_6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
