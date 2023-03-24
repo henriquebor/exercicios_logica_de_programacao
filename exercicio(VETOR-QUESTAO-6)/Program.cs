4bba0688834487d9f6da63391272b85343e332c1
Henrique Borges <henrique.borges.hb79@gmail.com>
2023-03-24 19:43:08 -0300

    atualização

--- exercicio(VETOR-QUESTAO-6)/Program.cs
+++ exercicio(VETOR-QUESTAO-6)/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace exercicio_VETOR_QUESTAO_6_
 {
@@ -10,6 +7,33 @@ namespace exercicio_VETOR_QUESTAO_6_
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] nomes = new string[n];
+            int[] idades = new int[n];
+
+            for (int i=0; i<n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+
+            }
+            int maiorIdade = idades[0];
+            int posicaoMaiorIdade = 0;
+
+            for (int i=1; i<n; i++)
+            {
+                if(idades[i] > maiorIdade)
+                {
+                    maiorIdade = idades[i];
+                    posicaoMaiorIdade = i;
+                }
+            }
+            Console.WriteLine("Pessoa mais velha: " + nomes[posicaoMaiorIdade]);
+            
+
+
         }
     }
 }
